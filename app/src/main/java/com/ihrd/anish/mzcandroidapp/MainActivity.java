abdea5b610b24b0d7335c2d52176d8fd87544b36
Anish S Nair <anish@Anishs-MacBook-Pro.local>
2019-01-15 11:44:48 +0530

    Button click Completed

--- app/src/main/java/com/ihrd/anish/mzcandroidapp/MainActivity.java
+++ app/src/main/java/com/ihrd/anish/mzcandroidapp/MainActivity.java
@@ -2,12 +2,38 @@ package com.ihrd.anish.mzcandroidapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+EditText ed1,ed2;
+Button b;
+String getUsername,getPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ed1=(EditText)findViewById(R.id.uname);
+        ed2=(EditText)findViewById(R.id.password);
+        b=(Button)findViewById(R.id.loginbutton);
+
+     b.setOnClickListener(new View.OnClickListener() {
+         @Override
+         public void onClick(View v) {
+
+             getUsername=ed1.getText().toString();
+             getPassword=ed2.getText().toString();
+
+             Toast.makeText(getApplicationContext(),getUsername,Toast.LENGTH_LONG).show();
+
+//             Toast.makeText(getApplicationContext(),"Hello",Toast.LENGTH_LONG).show();
+         }
+     });
+
+
     }
 }
